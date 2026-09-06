e1402557af2f263d6f578793306eeeef0c2d82ea
StarcevDA <isava2010@gmail.com>
2026-09-06 18:17:39 +0500

    Выполнены задания 1-4 по Terrafor

---
+++ src/disk_vm.tf
@@ -0,0 +1,48 @@
+# disk_vm.tf
+resource "yandex_compute_disk" "storage_disk" {
+  count = 3
+  name  = "storage-disk-${count.index + 1}"
+  type  = "network-hdd"
+  size  = 1
+  zone  = var.default_zone
+}
+
+resource "yandex_compute_instance" "storage" {
+  name        = "storage"
+  platform_id = "standard-v2"
+  zone        = var.default_zone
+
+  resources {
+    cores         = 2
+    memory        = 2
+    core_fraction = 5
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = data.yandex_compute_image.ubuntu.image_id
+    }
+  }
+
+  scheduling_policy {
+    preemptible = true
+  }
+
+  network_interface {
+    subnet_id          = yandex_vpc_subnet.develop.id
+    nat                = true
+    security_group_ids = [yandex_vpc_security_group.example.id]
+  }
+
+  dynamic "secondary_disk" {
+    for_each = yandex_compute_disk.storage_disk[*].id
+    content {
+      disk_id = secondary_disk.value
+    }
+  }
+
+  metadata = {
+    serial-port-enable = 1
+    ssh-keys           = "ubuntu:${local.ssh_public_key}"
+  }
+}
